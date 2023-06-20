ca316948476a6a66bd1d59b0d3c3965b3b76e9af
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-06-20 13:07:59 +0200

    pattern

--- src/components/ContactsForm/ContactForm.jsx
+++ src/components/ContactsForm/ContactForm.jsx
@@ -31,7 +31,7 @@ class PhoneBookContainer extends React.Component {
         onChange={this.currentName}
         type="text"
         name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        pattern="^[A-Za-z\u0080-\uFFFF ']+$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         />
@@ -40,7 +40,7 @@ class PhoneBookContainer extends React.Component {
         onChange={this.currentName}
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="^(\+?[0-9.\(\)\-\s]*)$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
         />
